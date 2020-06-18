19df4959ee3cd860e3f7e632b22eb2177599f040
manick <manickprime@gmail.com>
2020-06-18 22:27:20 +0530

    navBar is done
    :)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,18 +6,18 @@ const RESOURCES = {
 "assets/assets/ig_logo.png": "84455fbaae537ec62c7a3b0572f1b733",
 "assets/assets/linked_in.png": "a9f7af5796bd66458bb89dd1d78c38ae",
 "assets/assets/OpenSans-Bold.ttf": "1025a6e0fb0fa86f17f57cc82a6b9756",
-"assets/assets/removed.png": "d662044119197fc1cea6cdb039586817",
+"assets/assets/removed.png": "35e8478c63322ab207088fb723dd0c7f",
 "assets/assets/RobotoMono-Regular.ttf": "b4618f1f7f4cee0ac09873fcc5a966f9",
 "assets/FontManifest.json": "092802b4989bc4b23e1ce91174ff3f96",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "7f1df1ee47854388e684afc0845ea820",
+"assets/LICENSE": "ae5513ea28e9561c47c63cef2dfbaabf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "9aeda4c0247e8f85faaeaaaf1db04cb2",
 "/": "9aeda4c0247e8f85faaeaaaf1db04cb2",
-"main.dart.js": "d907b1f2ff9dce90e6d3cf43f62b8547",
+"main.dart.js": "5b07d986c6a5019263529182350e8406",
 "manifest.json": "2175942578d2b62dc0c979a227d608b1"
 };
 
